5b805ea6cd6d5ed5505f10f6e08f8566b6397213
Celeste Tanaka <cehmayumi@gmail.com>
2024-06-03 15:07:58 -0300

    ✨ feat: hero section adicionada e estilizada

---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,12 @@
+
+
+const HeroSection = ()=> {
+    return(
+        <section>
+            <div className="top-0  left-0 w-full h-full bg-black"></div>
+            <video className="h-full w-full object-cover" src="/ocean.mp4" autoPlay loop muted/>
+        </section>
+    )
+}
+
+export default HeroSection
