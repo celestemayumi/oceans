b04a386a1659c26b35eb694e46da4a6d282826ab
Celeste Tanaka <cehmayumi@gmail.com>
2024-06-04 18:21:38 -0300

    ✨ feat: Hero section da pagina de parceiros

--- src/app/parceiro/page.tsx
+++ src/app/parceiro/page.tsx
@@ -1,6 +1,16 @@
+import HeroSection from "@/components/HeroSection"
+
 export default function Parceiro(){
     return(
         <>
+        <HeroSection
+        source="/team.mp4"
+        title1="FAÇA PARTE"
+        title2="DO"
+        highlight="NOSSO TIME"
+        desc="Proteger nossos oceanos é essencial para o futuro do nosso planeta. Da preservação da biodiversidade marinha à promoção de práticas pesqueiras sustentáveis e à mitigação da poluição oceânica, cada ação conta. Ao projetar nossos oceanos, estamos moldando um mundo mais saudável e equilibrado para as gerações futuras."
+        button="SAIBA MAIS"
+        />
         </>
     )
 }
