14132806dc495cb8c53066d4666f5e113c4723db
Celeste Tanaka <cehmayumi@gmail.com>
2024-06-04 18:02:44 -0300

    🎨 feat: HeroSection reutilizavel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,17 @@ import HelpCard from "@/components/HelpCard";
 export default function Home() {
   return (
     <main>
-      <HeroSection/>
-      <Statistic/>
-      <OurProjects/>
-      <HelpCard/>
+      <HeroSection
+        source="/ocean.mp4"
+        title1="PROTEJA OS"
+        title2="NOSSOS"
+        highlight="OCEANOS"
+        desc="Proteger nossos oceanos é essencial para o futuro do nosso planeta. Da preservação da biodiversidade marinha à promoção de práticas pesqueiras sustentáveis e à mitigação da poluição oceânica, cada ação conta. Ao projetar nossos oceanos, estamos moldando um mundo mais saudável e equilibrado para as gerações futuras."
+        button="SAIBA MAIS"
+      />
+      <Statistic />
+      <OurProjects />
+      <HelpCard />
     </main>
   );
 }
--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,10 +1,13 @@
-const HeroSection = () => {
+import HeroSecProps from "./interface";
+
+
+const HeroSection = (props:HeroSecProps) => {
   return (
     <section>
       <div className="top-0  left-0 w-full h-full bg-black"></div>
       <video
         className="h-full w-full object-cover"
-        src="/ocean.mp4"
+        src={props.source}
         autoPlay
         loop
         muted
@@ -12,15 +15,15 @@ const HeroSection = () => {
       <div className="absolute w-full h-full top-0 flex flex-col justify-center text-white p-40 mt-20">
         <div className="mb-8 w-32 bg-[#93D6E2] h-6"></div>
         <div className="text-8xl font-bold leading-snug	">
-          <p>PROTEJA OS</p>
+          <p>{props.title1}</p>
           <p>
-            NOSSOS <span className="text-[#93D6E2]">OCEANOS</span>
+            {props.title2} <span className="text-[#93D6E2]">{props.highlight}</span>
           </p>
         </div>
         <p className="w-1/2 text-lg mt-10">
-        Proteger nossos oceanos é essencial para o futuro do nosso planeta. Da preservação da biodiversidade marinha à promoção de práticas pesqueiras sustentáveis e à mitigação da poluição oceânica, cada ação conta. Ao projetar nossos oceanos, estamos moldando um mundo mais saudável e equilibrado para as gerações futuras.
+        {props.desc}
         </p>
-        <button className="bg-[#f3f3f3] text-[#1D4B7F] w-1/5 font-bold rounded-md py-2 px-6 mt-16 text-2xl">SAIBA MAIS</button>
+        <button className="bg-[#f3f3f3] text-[#1D4B7F] w-1/5 font-bold rounded-md py-2 px-6 mt-16 text-2xl">{props.button}</button>
       </div>
     </section>
   );
