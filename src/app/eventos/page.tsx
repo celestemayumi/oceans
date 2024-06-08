de173881d11df5e2974a02565a166fd21ea7ce01
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2024-06-07 21:17:38 -0300

    :sparkles: feat: página de eventos

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -1,8 +1,18 @@
-import EventList from "@/components/EventList"
-export default function Eventos(){
-    return(
-        <>
-        <EventList/>
-        </>
-    )
+import EventList from "@/components/EventList";
+import EventCard from "@/components/EventCard";
+import ImageSection from "@/components/ImageSection";
+export default function Eventos() {
+  return (
+    <>
+      <ImageSection
+        source="/card2.png"
+        title1="PARTICIPE DE EVENTOS"
+        title2="COMO"
+        highlight="VOLUNTÁRIO"
+        desc="Estamos convidando voluntários apaixonados como você para se juntarem à nossa iniciativa de limpeza de praias, parte de nosso compromisso com a economia azul. Este é um chamado para todos que acreditam no poder da ação comunitária e no impacto positivo que podemos ter juntos."
+        button="FAÇA PARTE"
+      />
+      <EventList />
+    </>
+  );
 }
--- src/components/EventCard/index.tsx
+++ src/components/EventCard/index.tsx
@@ -1,15 +1,29 @@
 import "./styles.css";
+import Image from "next/image";
 
 const EventCard = (props: any) => {
   return (
-        <div className="event-card">
-            <h1 className="  mx-10 text-xl text-white font-semibold" id="title-event" >{props.title}</h1>
-            <p className=" mx-10 text-xl text-white font-semibold">{props.desc}</p>
-            <p className=" mx-10 text-xl text-white ">{props.local}</p>
-            <p className=" mx-10 text-xl text-white ">{props.data}</p>
-            <button className="py-4 w-1/3  mx-10 mt-10 bg-[#fff] text-black font-bold" onClick={props.click}>INSCREVA-SE</button>
-        </div>
-  
+    <div className="div-event-card ">
+      <div className="event-card ">
+        <h1
+          className=" drop-shadow-3xl mx-10 text-xl text-white font-semibold"
+          id="title-event"
+        >
+          {props.title}
+        </h1>
+        <p className="desc drop-shadow-3xl mx-10 text-xl text-white font-semibold">
+          {props.desc}
+        </p>
+        <p className=" mx-10 text-xl text-white ">{props.local}</p>
+        <p className=" mx-10 text-xl text-white ">{props.data}</p>
+        <button
+          className="py-4 w-1/3  mx-10 mt-10 bg-[#fff] text-black font-bold"
+          onClick={props.click}
+        >
+          INSCREVA-SE
+        </button>
+      </div>
+    </div>
   );
 };
 
